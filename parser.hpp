62b411fa9d6c84719274d6a72f83e15e933a5efe
Chega123 <85765351+Chega123@users.noreply.github.com>
2024-11-15 00:50:28 -0500

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -6,15 +6,28 @@
 #include <memory>
 using namespace std;
 
-struct ASTNode {  //Aqui esta la base son nodos con su nombre y un vector de punteors a ASTNodes
-    string token_name;
-    vector<unique_ptr<ASTNode>> children;
+struct ASTNode {
+    std::string token_name;
+    std::vector<ASTNode> children;
 
-    ASTNode(string token) : token_name(token) {}
-    void addChild(unique_ptr<ASTNode> child) {
-        children.push_back(move(child));
+    // Constructor por defecto que inicializa token_name vacío
+    ASTNode() : token_name("") {}
+
+    // Constructor que acepta un nombre de token
+    ASTNode(std::string token) : token_name(std::move(token)) {}
+
+    void addChild(ASTNode child) {
+        children.push_back(std::move(child));
+    }
+
+    // Función isEmpty para verificar si el nodo tiene hijos
+    bool isEmpty() const {
+        return children.empty();  // Retorna true si no tiene hijos
     }
 };
+
+
+
 
 struct token {
     string token_name;
@@ -28,61 +41,61 @@ struct token {
 
 class Parser {
 public:
-  Parser(const std::vector<token>& tokens);
-  bool parse(unique_ptr<ASTNode>& node);
+    Parser(const std::vector<token>& tokens);
+    bool parse(ASTNode& node);
 
 private:
-  std::vector<token> tokens;
-  size_t current;
+    std::vector<token> tokens;
+    size_t current;
 
-  token currToken();
-  void syncToDelimiter();
-  token nextToken();
-  void fail(string message);
-  bool nonTerminal(std::string name);
-  bool Program(unique_ptr<ASTNode>& node);
-  bool ProgramPrime(unique_ptr<ASTNode>& node);
-  bool Declaration(unique_ptr<ASTNode>& node);
-  bool VarDecl(unique_ptr<ASTNode>& node);
-  bool VarDeclPrime(unique_ptr<ASTNode>& node, unique_ptr<ASTNode> identifierNode);
-  bool Type(unique_ptr<ASTNode>& node);
-  bool TypePrime(unique_ptr<ASTNode>& node);
-  bool BasicType(unique_ptr<ASTNode>& node);
-  bool Function(unique_ptr<ASTNode>& node);
-  bool Params(unique_ptr<ASTNode>& node);
-  bool ParamList(unique_ptr<ASTNode>& node);
-  bool ParamListPrime(unique_ptr<ASTNode>& node);
-  bool CompoundStmt(unique_ptr<ASTNode>& node);
-  bool StmtList(unique_ptr<ASTNode>& node);
-  bool StmtListPrime(unique_ptr<ASTNode>& node);
-  bool Statement(unique_ptr<ASTNode>& node);
-  bool IfStmt(unique_ptr<ASTNode>& node);
-  bool ForStmt(unique_ptr<ASTNode>& node);
-  bool AuxIf(unique_ptr<ASTNode>& node);
-  bool ReturnStmt(unique_ptr<ASTNode>& node);
-  bool PrintStmt(unique_ptr<ASTNode>& node);
-  bool ExprStmt(unique_ptr<ASTNode>& node);
-  bool ExprList(unique_ptr<ASTNode>& node);
-  bool ExprListPrime(unique_ptr<ASTNode>& node);
-  bool Expression(unique_ptr<ASTNode>& node);
-  bool AuxExpression(unique_ptr<ASTNode>& node);
-  bool OrExpr(unique_ptr<ASTNode>& node);
-  bool OrExprPrime(unique_ptr<ASTNode>& node);
-  bool AndExpr(unique_ptr<ASTNode>& node);
-  bool AndExprPrime(unique_ptr<ASTNode>& node);
-  bool EqExpr(unique_ptr<ASTNode>& node);
-  bool EqExprprime(unique_ptr<ASTNode>& node);
-  bool RelExpr(unique_ptr<ASTNode>& node);
-  bool RelExprPrime(unique_ptr<ASTNode>& node);
-  bool Expr(unique_ptr<ASTNode>& node);
-  bool ExprPrime(unique_ptr<ASTNode>& node);
-  bool Term(unique_ptr<ASTNode>& node);
-  bool TermPrime(unique_ptr<ASTNode>& node);
-  bool Unary(unique_ptr<ASTNode>& node);
-  bool Factor(unique_ptr<ASTNode>& node);
-  bool FactorPrime(unique_ptr<ASTNode>& node);
-  bool Primary(unique_ptr<ASTNode>& node);
-  bool AuxPrimary(unique_ptr<ASTNode>& node);
+    token currToken();
+    void syncToDelimiter();
+    token nextToken();
+    void fail(string message);
+    bool nonTerminal(std::string name);
+    bool Program(ASTNode& node);
+    bool ProgramPrime(ASTNode& node);
+    bool Declaration(ASTNode& node);
+    bool VarDecl(ASTNode& node);
+    bool VarDeclPrime(ASTNode& node, ASTNode identifierNode);
+    bool Type(ASTNode& node);
+    bool TypePrime(ASTNode& node);
+    bool BasicType(ASTNode& node);
+    bool Function(ASTNode& node);
+    bool Params(ASTNode& node);
+    bool ParamList(ASTNode& node);
+    bool ParamListPrime(ASTNode& node);
+    bool CompoundStmt(ASTNode& node);
+    bool StmtList(ASTNode& node);
+    bool StmtListPrime(ASTNode& node);
+    bool Statement(ASTNode& node);
+    bool IfStmt(ASTNode& node);
+    bool ForStmt(ASTNode& node);
+    bool AuxIf(ASTNode& node);
+    bool ReturnStmt(ASTNode& node);
+    bool PrintStmt(ASTNode& node);
+    bool ExprStmt(ASTNode& node);
+    bool ExprList(ASTNode& node);
+    bool ExprListPrime(ASTNode& node);
+    bool Expression(ASTNode& node);
+    bool AuxExpression(ASTNode& node);
+    bool OrExpr(ASTNode& node);
+    bool OrExprPrime(ASTNode& node);
+    bool AndExpr(ASTNode& node);
+    bool AndExprPrime(ASTNode& node);
+    bool EqExpr(ASTNode& node);
+    bool EqExprprime(ASTNode& node);
+    bool RelExpr(ASTNode& node);
+    bool RelExprPrime(ASTNode& node);
+    bool Expr(ASTNode& node);
+    bool ExprPrime(ASTNode& node);
+    bool Term(ASTNode& node);
+    bool TermPrime(ASTNode& node);
+    bool Unary(ASTNode& node);
+    bool Factor(ASTNode& node);
+    bool FactorPrime(ASTNode& node);
+    bool Primary(ASTNode& node);
+    bool AuxPrimary(ASTNode& node);
 };
 
 #endif // PARSER_HPP
